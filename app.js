33b6087270de9b7e229e1b4e76d2edf77c1984c5
Matt Dotson <mdotson@microsoft.com>
2016-09-21 13:21:17 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var botauth = function() {
+
+};
+
+botauth.prototype.middleware = function() {
+    var self = this;
+    
+    return function(session, next) {
+
+    };
+}
+
+module.exports = botauth;
