33a4572336cb8f02c51abbc5c24a65bd5ef1ca21
Matt Dotson <mdotson@microsoft.com>
2016-09-23 12:03:59 -0700

    added tests

---
+++ test/index-tests.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const botauth = require('../lib/index');
+
+describe('botauth', function() {
+    describe('#configure', function() {
+        it('should error if bot is not passed', function() {
+            assert.throws(function() {
+                botauth.configure();
+            });
+        });
+    });
+});
