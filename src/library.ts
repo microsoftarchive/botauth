4cc63ed1d1f3f3990e9b828b68f4f23f495680c6
Matt Dotson <mdotson@microsoft.com>
2016-10-05 01:40:31 -0700

    fixed library naming

--- src/library.ts
+++ src/library.ts
@@ -6,24 +6,25 @@ const dialogName : string = "auth";
 export const name : string = `${libraryName}:${dialogName}`;
 
 export function build() {
-    let authlib = new builder.Library(name);
-    authlib.dialog("auth", new builder.SimpleDialog(function(session : builder.Session, args : any) {
+    let authlib = new builder.Library(libraryName);
+    authlib.dialog(dialogName, new builder.SimpleDialog(function(session : builder.Session, args : any) {
 
-        console.log(`${name} args = %j`, args);
-        console.log("[botauth:auth] userData = %j", session.userData);
+        console.log(`[${name}] args = %j`, args);
+        console.log(`[${name}] userData = %j`, session.userData);
         let providerId = args.providerId;
         let authUrl = args.authUrl;
 
         if(session.userData && session.userData.botauth && session.userData.botauth.tokens && session.userData.botauth.tokens[providerId]) {
-            console.log("[botauth:auth] resumed");
+            console.log(`[${name}] resumed`);
             session.endDialog("thanks. you're signed in");
         } else {
-            console.log("[botauth:auth] started");
+            console.log(`[${name}] started`);
+            let state : string = Buffer.from(JSON.stringify(session.message.address)).toString('base64');
             var msg = new builder.Message(session)
                 .attachments([ 
                     new builder.SigninCard(session)
                         .text("Connect to OAuth Provider") 
-                        .button("connect", "https://botauth.ngrok.io/botauth/auth?address=" + Buffer.from(JSON.stringify(session.message.address)).toString('base64'))
+                        .button("connect", `${authUrl}?state=${state}`)
                 ]);
 
             session.send(msg); 
